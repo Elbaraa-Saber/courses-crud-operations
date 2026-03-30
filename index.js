90689ab60481ed51e5e9e50b64bfc8fa974ab35c
Elbaraa-Saber <baraax01@gmail.com>
2026-03-30 19:20:28 +0700

    Api post course with validation for body (json info)

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express');
 // start aplication
 const app = express();
 
+// CRUD (Create, Read, Update, Delete) operations
+app.use(express.json()); // or body-parser
+
+// joi or express-validator or zod-dev for validation 
+
+const { body, validationResult  } = require('express-validator');
+const { count } = require('node:console');
+
+
 const courses = [
     {
         id: 1,
@@ -32,6 +41,34 @@ app.get('/api/courses/:courseId', (req, res) => {
     }
     res.json(course);
 });
+
+
+// create a new course
+app.post('/api/courses', 
+    [
+        body('title')
+            .notEmpty()
+            .withMessage('Title is required')
+            .isLength({min:2})
+            .withMessage('Title at least 2 characters long'),
+        body('price')
+            .notEmpty()
+            .isInt()
+            .withMessage('Price is required')
+    ], (req, res) => 
+    {
+    console.log(req.body);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+        res.status(400).json({errors: errors.array()});
+        return; 
+    }
+    const course = {id: courses.length + 1, ...req.body};
+    courses.push(course);
+    res.json(course).status(201);
+})
+
 
 
 app.listen(4000, () => {
