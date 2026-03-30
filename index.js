d69d9389ea4bde539c8e1550608cd1dfca8f6cbd
Elbaraa-Saber <baraax01@gmail.com>
2026-03-30 19:16:51 +0700

    Api to Get all courses

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const express = require('express');
 // start aplication
 const app = express();
 
-app.use(express.json()); // or body-parser
-
-// joi or express-validator or zod-dev for validation 
-
-const { body, validationResult  } = require('express-validator');
-const { count } = require('node:console');
-
 const courses = [
     {
         id: 1,
@@ -23,49 +16,10 @@ const courses = [
     },
 ]
 
-// CRUD (Create, Read, Update, Delete) operations
-
 // get all courses
 app.get('/api/courses', (req, res) => {
     res.json(courses);
 });
-
-// get course by id
-app.get('/api/courses/:courseId', (req, res) => {
-    const courseId = parseInt(req.params.courseId);
-    const course = courses.find((course) => course.id === courseId);
-    if (!course){
-        res.status(404).json({message: 'Course not fournd'});
-        return;
-    }
-    res.json(course);
-});
-
-// create a new course
-app.post('/api/courses', 
-    [
-        body('title')
-            .notEmpty()
-            .withMessage('Title is required')
-            .isLength({min:2})
-            .withMessage('Title at least 2 characters long'),
-        body('price')
-            .notEmpty()
-            .isInt()
-            .withMessage('Price is required')
-    ], (req, res) => 
-    {
-    console.log(req.body);
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()){
-        res.status(400).json({errors: errors.array()});
-        return; 
-    }
-    const course = {id: courses.length + 1, ...req.body};
-    courses.push(course);
-    res.json(course).status(201);
-})
 
 
 app.listen(4000, () => {
