0825a0e2e6a9234cda4d92ecb36d1955e0e8a529
Elbaraa-Saber <baraax01@gmail.com>
2026-04-01 14:29:08 +0700

    Organize the project by transferring the data to a separate file.

---
+++ data/courses.js
@@ -0,0 +1,14 @@
+const courses = [
+    {
+        id: 1,
+        title: "Js course", 
+        price: 100
+    },
+    {
+        id: 2,
+        title: "React course", 
+        price: 800
+    },
+]
+
+module.exports = courses;
--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const { body, validationResult  } = require('express-validator');
 const { count } = require('node:console');
 
 
-const courses = [
-    {
-        id: 1,
-        title: "Js course", 
-        price: 100
-    },
-    {
-        id: 2,
-        title: "React course", 
-        price: 800
-    },
-]
+const { courses} = require('./data/courses');
 
 // get all courses
 app.get('/api/courses', (req, res) => {
